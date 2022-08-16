82fd43730aba1aaaf63455b4a7794b89e3d69005
dariakochanova <kochanova.d.a@gmail.com>
2022-08-16 17:20:52 +0300

    add domain components

---
+++ app/src/main/java/com/hfad/shoppinglist/domain/ShopListRepository.kt
@@ -0,0 +1,9 @@
+package com.hfad.shoppinglist.domain
+
+interface ShopListRepository {
+    fun addShopItem(shopItem: ShopItem)
+    fun deleteShopItem(shopItem: ShopItem)
+    fun editShopItem(shopItem: ShopItem)
+    fun getShopItem(shopItem: ShopItem): ShopItem
+    fun getShopList(): List<ShopItem>
+}

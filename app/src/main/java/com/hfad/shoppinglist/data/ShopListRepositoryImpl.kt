54be0b113d5d1ef854037cee3382ff46633a70fd
dariakochanova <kochanova.d.a@gmail.com>
2022-08-21 21:04:31 +0300

    add latest changes

--- app/src/main/java/com/hfad/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/hfad/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -1,26 +1,38 @@
 package com.hfad.shoppinglist.data
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.hfad.shoppinglist.domain.ShopItem
 import com.hfad.shoppinglist.domain.ShopListRepository
 import java.lang.RuntimeException
 
 object ShopListRepositoryImpl: ShopListRepository {
 
+    private val shopListLD = MutableLiveData<List<ShopItem>>()
     private val shopList = mutableListOf<ShopItem>()
     private var autoIncrementId = 0
+
+    init {
+        for (i in 0 until 10){
+            val item = ShopItem("Name $i", i, true)
+            addShopItem(item)
+        }
+    }
 
     override fun addShopItem(shopItem: ShopItem) {
         if (shopItem.id == ShopItem.UNDEFIND_ID){
         shopItem.id = autoIncrementId++
         }
         shopList.add(shopItem)
-    }
-
-    override fun editShopItem(shopItem: ShopItem) {
-        shopList.remove(shopItem)
+        updateList()
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+        updateList()
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
         val oldElement = getShopItem(shopItem.id)
         shopList.remove(oldElement)
         addShopItem(shopItem)
@@ -32,8 +44,12 @@ object ShopListRepositoryImpl: ShopListRepository {
         } ?: throw RuntimeException("Element with id $shopItemId not found")
     }
 
-    override fun getShopList(): List<ShopItem> {
-        return shopList.toMutableList()
+    override fun getShopList(): LiveData<List<ShopItem>> {
+        return shopListLD
+    }
+
+    private fun updateList(){
+        shopListLD.value = shopList.toList()
     }
 
 
--- app/src/main/java/com/hfad/shoppinglist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/hfad/shoppinglist/domain/GetShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.hfad.shoppinglist.domain
 
 class GetShopItemUseCase(private val shopListRepository: ShopListRepository){
-    fun getShopItem(shopItem: ShopItem): ShopItem{
-        return shopListRepository.getShopItem(shopItem)
+    fun getShopItem(shopItemId: Int): ShopItem{
+        return shopListRepository.getShopItem(shopItemId)
     }
 }
